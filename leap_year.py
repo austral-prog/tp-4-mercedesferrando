e83e3b1be8259de2c7a1ff565b405b449e2a9e8d
mercedesferrando <mferrando@mail.austral.edu.ar>
2025-04-20 19:56:39 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,6 +1,10 @@
-def leap_year():
-    year = int(input("Ingrese un año: "))
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        print(f'El año {year} es bisiesto')
+def es_bisiesto(ano):
+    if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+        return True
     else:
-        print(f'El año {year} no es bisiesto')
+        return False
+ano = int(input("Ingrese un año: "))
+if es_bisiesto(ano):
+    print(f"El año {ano} es bisiesto")
+else:
+    print(f"El año {ano} no es bisiesto")
